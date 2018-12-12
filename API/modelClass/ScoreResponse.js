33cf60c81588293c12a113c5e6fa63a6e6e28896
Sunidhi Kabra <kabra.sunidhi14.com>
2018-12-12 15:38:56 -0500

    general scoreboard implemented

---
+++ API/modelClass/ScoreResponse.js
@@ -0,0 +1,10 @@
+class ScoreResponse{
+
+  constructor(scores, exhibit, evaluator){
+    this.scores = scores;
+    this.exhibit = exhibit;
+    this.evaluator = evaluator;
+  }
+}
+
+module.exports = ScoreResponse;
